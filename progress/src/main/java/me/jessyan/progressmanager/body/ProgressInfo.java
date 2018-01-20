837bce2653175965ac050502683169efc7336286
JessYan <jessyan@foxmail.com>
2018-01-20 11:56:52 +0800

    Improve comment

--- progress/src/main/java/me/jessyan/progressmanager/body/ProgressInfo.java
+++ progress/src/main/java/me/jessyan/progressmanager/body/ProgressInfo.java
@@ -33,7 +33,7 @@ public class ProgressInfo implements Parcelable {
     private long intervalTime; //本次调用距离上一次被调用所间隔的时间(毫秒)
     private long eachBytes; //本次调用距离上一次被调用的间隔时间内上传或下载的byte长度
     private long id; //如果同一个 Url 地址,上一次的上传或下载操作都还没结束,
-    //又开始了新的上传或下载操作(比如用户点击多次点击上传或下载同一个 Url 地址,当然你也可以在上层屏蔽掉用户的重复点击),
+    //又开始了新的上传或下载操作(比如用户多次点击上传或下载同一个 Url 地址,当然你也可以在上层屏蔽掉用户的重复点击),
     //此 id (请求开始时的时间)就变得尤为重要,用来区分正在执行的进度信息,因为是以请求开始时的时间作为 id ,所以值越大,说明该请求越新
     private boolean finish; //进度是否完成
 
