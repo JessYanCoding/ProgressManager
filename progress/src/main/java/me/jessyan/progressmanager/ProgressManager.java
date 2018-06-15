f8c055288c9ee00c527ce5464eea8d90042f6068
JessYan <jessyan@foxmail.com>
2018-06-15 11:48:43 +0800

    Improve ProgressManager

--- progress/src/main/java/me/jessyan/progressmanager/ProgressManager.java
+++ progress/src/main/java/me/jessyan/progressmanager/ProgressManager.java
@@ -121,6 +121,7 @@ public final class ProgressManager {
      * @param refreshTime 间隔时间,单位毫秒
      */
     public void setRefreshTime(int refreshTime) {
+        if (refreshTime < 0 ) throw new IllegalArgumentException("refreshTime must be >= 0");
         this.mRefreshTime = refreshTime;
     }
 
@@ -187,6 +188,7 @@ public final class ProgressManager {
      * @return
      */
     public OkHttpClient.Builder with(OkHttpClient.Builder builder) {
+        checkNotNull(builder, "builder cannot be null");
         return builder
                 .addNetworkInterceptor(mInterceptor);
     }
