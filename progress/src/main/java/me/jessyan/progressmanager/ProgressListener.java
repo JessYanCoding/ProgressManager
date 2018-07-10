51460bba412b1f9d21d602372eae36159633e502
JessYan <jessyan@foxmail.com>
2018-07-10 14:55:20 +0800

    Improve license

--- progress/src/main/java/me/jessyan/progressmanager/ProgressListener.java
+++ progress/src/main/java/me/jessyan/progressmanager/ProgressListener.java
@@ -1,12 +1,12 @@
-/**
+/*
  * Copyright 2017 JessYan
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -36,7 +36,7 @@ public interface ProgressListener {
      * 错误监听
      *
      * @param id 进度信息的 id
-     * @param e 错误
+     * @param e  错误
      */
     void onError(long id, Exception e);
 }
--- progress/src/main/java/me/jessyan/progressmanager/ProgressManager.java
+++ progress/src/main/java/me/jessyan/progressmanager/ProgressManager.java
@@ -1,12 +1,12 @@
-/**
+/*
  * Copyright 2017 JessYan
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -121,7 +121,7 @@ public final class ProgressManager {
      * @param refreshTime 间隔时间,单位毫秒
      */
     public void setRefreshTime(int refreshTime) {
-        if (refreshTime < 0 ) throw new IllegalArgumentException("refreshTime must be >= 0");
+        if (refreshTime < 0) throw new IllegalArgumentException("refreshTime must be >= 0");
         this.mRefreshTime = refreshTime;
     }
 
